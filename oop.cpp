ea30085877de8683e98085e4135471ae8642adbf
SOVETSKAYA-CHERDACHNAYA-TABURETKA <rufusalastor@gmail.com>
2023-10-29 22:03:18 +0300

    pointers be pointin, output issue resolved

--- oop.cpp
+++ oop.cpp
@@ -3,6 +3,17 @@
 using namespace std;
 
 class point{
+public:
+point(int val_x, int val_y){
+    x = val_x;
+    y = val_y;
+    cout << this << " sdelal";
+}
+point(){
+x = 0;
+y = 0;
+    cout << this << " umer";
+};
 private:
 int x;
 int y;
@@ -22,16 +33,32 @@ void sety(int valy){
 };
 
 class grinder{
+private:
+bool chvolt(){
+    
+return true;
+};
 public:
 void start(){
-    cout << "a"<< endl;
+    setlocale(LC_ALL, "en");
+    bool voltnorm = chvolt();
+    if (voltnorm ==  true){
+    cout << "ahhhh " << "..."<< endl;
+    }
+    else{
+    cout << "critikal error " << endl;
+    };
+    
 };
 
 };
+
 
 int main(){
-    point a;
-    return 0;
     grinder vitek;
     vitek.start();
+    point a(9,9);
+
+    return 0;
+    
 };
