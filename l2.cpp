e81097c2e80e538a22f1599cafe1a38fb656b3e2
SOVETSKAYA-CHERDACHNAYA-TABURETKA <rufusalastor@gmail.com>
2023-11-18 17:47:46 +0300

    cs coure shenanigans be like

---
+++ l2.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+using namespace std;
+int main()
+{
+    /*
+	float a;
+	float b;
+	cout << "Insert at least 2 numbers please " << endl;
+	cin >> a;
+	cin >> b;
+	if (a - b == 0)
+	{
+		cout << "those are aqual";
+	}
+	else
+	{
+		cout << "those are different" << endl;
+        cout << "the difference is " << abs(a-b) << endl;
+        cout << "the arythmetical median is " << (a+b)/2 << endl;
+        cout << "the geometrical median is " << sqrt(a*b) << endl;
+	}*/
+    /*
+    float c;
+    cout << "insert a number from 0 to 100 " << endl;
+    cin >> c;
+    if ((c>=0)&&(c<50))
+    {
+        cout << "it is in the range and is less than 50"<<endl;
+    }
+    else if (c==50)
+    {
+        cout << "it is precisely 50"<< endl;
+    }
+    else if ((c>=50)&&(c<=100))
+    {
+        cout << "it is in the range and more than 50"<< endl;
+    }
+    else cout << "the number is out of range or undefined";
+*/
+/*
+cout << "insert 2 numbers for an operation" << endl;
+float a;
+float b;
+cin >> a;
+cin >> b;
+cout << "select an operation from the following list: +; -; /; * " << endl;
+char act;
+cin >> act;
+switch (act)
+{
+    case '+':
+    cout << a+b << endl;
+    break;
+    case '-':
+    cout << a-b << endl;
+    break;
+    case '/':
+    cout << a/b << endl;
+    break;
+    case '*':
+    cout << a*b << endl;
+    break;
+}
+*/
+    return 0;
+};
